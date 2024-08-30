2c82c26e039207a051132cf5c47dff5850ba887c
Markus Wirtz <info@markuswirtz.net>
2024-08-30 14:19:35 +0200

    Initial Commit

---
+++ WhatsAppScreenTest/WhatsAppScreenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppScreenTestApp.swift
+//  WhatsAppScreenTest
+//
+//  Created by Markus Wirtz on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppScreenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
